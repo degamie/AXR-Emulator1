7ce8b60a1f1acac267fa00416f154ef22408a7c0
Degamie_sign <degamiesign@gmail.com>
2024-12-05 09:23:25 +0530

    Tweleth Commit

--- app/src/main/java/com/example/axremulator2/Common/Helpers/PlaneRenderer.java
+++ app/src/main/java/com/example/axremulator2/Common/Helpers/PlaneRenderer.java
@@ -1,4 +1,24 @@
 package com.example.axremulator2.Common.Helpers;
 
+import android.graphics.Mesh;
+import android.graphics.Shader;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 public class PlaneRenderer {
+    private Mesh meshobj;
+    private IndexBuffer indexBuffer;
+    private VertexBuffer vertexBuffer;
+    private final Shader shader;
+
+    private FloatBuffer vertexBuffer=
+            ByteBuffer.allocateDirect(INITIAL_VERTEX_BUFFER_SIZE)
+                    .order(ByteOrder.nativeOrder())
+                    .asFloatBuffer()
+                    .indexBuffer=ByteBuffer.allocateDirect(INITIAL_VERTEX_BUFFER_SIZE_BYTES);
+                    .order(ByteOrder.nativeOrder())
+                    .asIntBuffer()
+
 }
