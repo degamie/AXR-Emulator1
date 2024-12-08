7c915ca8656041fc56b94a4ab3b887006405deb2
Degamie_sign <degamiesign@gmail.com>
2024-12-08 18:06:37 +0530

    Seventeeth Commit

--- app/src/main/java/com/example/axremulator2/Common/Helpers/DisplayRotationHelper.java
+++ app/src/main/java/com/example/axremulator2/Common/Helpers/DisplayRotationHelper.java
@@ -6,15 +6,27 @@ import android.hardware.display.DisplayManager;
 import android.view.Display;
 
 public class DisplayRotationHelper {
-    public DisplayManager displayManager;
+    public DisplayManager  displayManager;
     public CameraManager cameraManager;
     public Display display;
-    public boolean ViewPort;
+    public boolean ViewPortChanged;
     public int viewHeight;
     public int viewWidth;
 
     public DisplayRotationHelper(Context context){
         displayManager =(displayManager)context.getSystemService(context.DISPLAY_SERVICE);
         cameraManager=(cameraManager)context.getSystemService(context.CAMERA_SERVICE);
+    }
+    public void onResume(){
+        displayManager.registerDisplayListener(this,null);
+
+    }
+    public void onPause(){
+        displayManager.unregisterDisplayListener(this);
+    }
+    public void onSurfaceChanged(int width ,int height){
+        viewWidth=width;
+        viewHeight=height;
+        ViewPortChanged=true;
     }
 }
