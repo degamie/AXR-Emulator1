d911513abf8bcafb17cd2c83c34fe03904735df7
Degamie_sign <106448560+degamie@users.noreply.github.com>
2024-12-12 08:35:12 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Common/Helpers/PlaneRenderer.java
+++ app/src/main/java/com/example/axremulator2/Common/Helpers/PlaneRenderer.java
@@ -1,16 +1,22 @@
 package com.example.axremulator2.Common.Helpers;
 
+import android.graphics.Camera;
 import android.graphics.Mesh;
 import android.graphics.Shader;
 
 import com.google.android.filament.IndexBuffer;
 import com.google.android.filament.VertexBuffer;
 import com.google.ar.core.Plane;
+import com.google.ar.core.Pose;
+import com.google.ar.core.TrackingState;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PlaneRenderer {
@@ -36,5 +42,13 @@ public class PlaneRenderer {
     public static float [] normalVector=new float[3];
 
     public final Map<Plane,Integer> planeIndexMap=new HashMap<>();
+    public void DrawPlanes(SampleRenderer sampleRenderer, Collection<Plane>allPlanes, Pose CameraPose,float [] cameraProjection ){
+        List<SortablePlane> sortablePlanes=new ArrayList<>();
+        for(Plane plane:allPlanes){
+            if(plane.getTrackingState()!= TrackingState.TRACKING || plane.getSubsumedBy()!=null){continue;}
+            float distance=calculateDistanceToPlane(plane.getCenterPose(),CameraPose);
+    }
 
 }
+
+    private float calculateDistanceToPlane(Pose centerPose, Pose cameraPose) {}
