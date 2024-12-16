e1007756a57863e6ed93458d32e9f37f762b0db9
Degamie_sign <106448560+degamie@users.noreply.github.com>
2024-12-16 08:38:45 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Common/Helpers/SampleRenderer.java
+++ app/src/main/java/com/example/axremulator2/Common/Helpers/SampleRenderer.java
@@ -2,6 +2,7 @@ package com.example.axremulator2.Common.helpers;
 
 import static android.text.method.TextKeyListener.clear;
 
+import android.annotation.SuppressLint;
 import android.content.res.AssetManager;
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
@@ -20,26 +21,27 @@ public class SampleRenderer {
     public int ViewPortWidth;
     public int ViewPortHeight;
 
-    public void SampleRenderer(GLSurfaceView glSurfaceView,Renderer renderer,AssetManager assetManager){
+    @SuppressLint("NotConstructor")
+    public void SampleRenderer(GLSurfaceView glSurfaceView, Renderer renderer, AssetManager assetManager){
         this.assetManager=assetManager;
         glSurfaceView.setPreserveEGLContextOnPause(true);
         glSurfaceView.setEGLContextClientVersion(3);
         glSurfaceView.setEGLConfigChooser(8,8,8,180);
+        GLES30 gles31 = gles30;
         glSurfaceView.setRenderer(
                 new GLSurfaceView.Renderer() {
                     @Override
                     public void onSurfaceCreated(GLES30 gl30, EGLConfig eglConfig) {
                         GLES30.glEnable(GLES30.GL_BLEND);
-                        GLError.maybeThrownError("Failed toe Enable Blending","glEnable");
+                        GLES30.glGetError();
                         renderer.onSurfaceCreated(SampleRenderer.this);
                     }
                 }
         @Override
         public void onSampleRendererr(GLES30 gles30){
 
-            clear(null,0f,0f,0f,0f);
+            clear(null);
             renderer.onDrawFrame(SampleRenderer.this);
-
                 }
 }
     public static interface Renderer{
@@ -62,9 +64,13 @@ public class SampleRenderer {
         }
             else{
                 framebufferId=frameBuffer.getFrameBufferId();
-                ViewPortWidth=frameBuffer.getWidth();
+                ViewPortWidth=frameBuffer.getViewPortWidth();
                 ViewPortHeight=frameBuffer.getHeight();
             }
+            GLES30.glBindFramebuffer(GLES30.GL_FRAMEBUFFER,framebufferId);
+            GLError.maybeThrownException("Unbale to Find FrameBuffer ","glFindFrameBuffer");
+            GLES30.glViewport(0,0,ViewPortWidth,ViewPortHeight);
+        GLError.maybeThrownGLException("Unbale to Find Dimension's ViePort Settings ","glViewport");
 
     }
 
