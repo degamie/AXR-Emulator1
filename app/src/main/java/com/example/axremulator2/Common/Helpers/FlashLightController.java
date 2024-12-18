f4937e703cbead71156a7fa60ca9c0dec167161b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2024-12-18 08:05:44 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Common/Helpers/FlashLightController.java
+++ app/src/main/java/com/example/axremulator2/Common/Helpers/FlashLightController.java
@@ -1,24 +1,28 @@
 package com.example.axremulator2.Common.helpers;
 
+import com.example.axremulator2.MainActivity;
 import static android.app.ProgressDialog.show;
 import static android.content.Context.CAMERA_SERVICE;
 import static android.content.pm.PackageManager.FEATURE_CAMERA_ANY;
+
+import static com.example.axremulator2.R.id.FlashLightEnableOn;
 
 import android.content.pm.PackageManager;
 import android.graphics.Camera;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
-import android.health.connect.datatypes.units.Length;
+//import android.health.connect.datatypes.units.Length;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.example.axremulator2.MainActivity;
 import com.example.axremulator2.R;
-
-import java.util.List;
+//
+//import java.util.List;
 public class FlashLightController extends CameraPermissionHelper{
     public MainActivity mainActivity;
     public Long CameraId=null;
@@ -26,11 +30,16 @@ public class FlashLightController extends CameraPermissionHelper{
     public DisplayRotationHelper displayRotationHelper;
     public Object surfaceView;
     public TapHelper tapHelper;
-    public ToggleButton toggle_flashlightbtn= toggle_flashlightbtn.findViewById(R.id.FlashLightEnableOn);
+    public ToggleButton toggle_flashlightbtn= toggle_flashlightbtn.findViewById(FlashLightEnableOn);
     public CameraManager cameraManager;
     public String getCameraid;
-    public Camera getCameraid(Long CameraId){
-        return CameraManager.class.getComponentType(CameraId);
+//    public Switch  flashableLightSwitch=R.id.surfaceview(FlashLightEnableOn);
+    public Integer getCameraid(Integer CameraId) throws CameraAccessException {
+            if(CameraId!=null) {CameraId=CameraManager.getCameraIdList(CameraId);}
+            else {
+                CameraId=-1;
+        }
+            return CameraId;
     }
 
     public int setContentView(int contentView) {
@@ -39,9 +48,9 @@ public class FlashLightController extends CameraPermissionHelper{
     }
     protected void OnCreate(Bundle SavedInstanceState,Switch flashableLightSwitch){
 //    Switch flashableLightSwitch;
-    flashableLightSwitch=R.id.surfaceview(R.id.FlashLightEnableOn);
-    CameraManager cameraManager=cameraManager.openCamera(cameraPermissionHelper.this);
 
+    CameraManager cameraManager=cameraManager.openCamera(cameraPermissionHelper.this);
+    flashableLightSwitch=R.id.surfaceview(FlashLightEnableOn);
     if(cameraPermissionHelper.hasCameraPermission(FEATURE_CAMERA_ANY || (PackageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)))){
             displayRotationHelper=new DisplayRotationHelper(this);
         }
@@ -50,8 +59,29 @@ public class FlashLightController extends CameraPermissionHelper{
         }
         setContentView(R.layout.activity_main_axractivity);
         surfaceView=R.id.findById(R.id.surfaceview);
-        displayRotationHelper=new DisplayRotationHelper();
+        displayRotationHelper=new DisplayRotationHelper(displayRotationHelper);
         TapHelper tapHelper;
+        flashableLightSwitch.setEnabled(true);
+        Toast FlashLightCamera=Toast.makeText(mainActivity.this,"Camera has FlashLight",Toast.LENGTH_SHORT).show();
+        Switch finalFlashableLightSwitch = flashableLightSwitch;
+        flashableLightSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (compoundButton.isChecked()) {
+                    try {
+                        cameraManager.setTorchMode("0", false);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                        finalFlashableLightSwitch.setText("Flash Tuned ONN");
+                    }
+                }
+                    else {
+                        finalFlashableLightSwitch.setText("Flash Tuned OFF");
+                    }
+
+                }
+
+        });
     }
 
     //Exceptional Handling
@@ -60,7 +90,7 @@ public class FlashLightController extends CameraPermissionHelper{
         if(toggle_flashlightbtn.isChecked()){
             try{
                 cameraManager.setTorchMode(getCameraid,true){
-                    Toast.makeText(MainActivity.this,"FlashLight is Tunred Onn");
+                    Toast flashLightIsTunredOnn = Toast.makeText(mainActivity.this, "FlashLight is Tunred Onn");
                     Toast.LENGTH_SHORT().show();
                 }
             }
@@ -71,9 +101,8 @@ public class FlashLightController extends CameraPermissionHelper{
       else{
           try{
                 cameraManager.setTorchMode(getCameraid,true){
-                    Toast.makeText(MainActivity.this,"FlashLight is Tunred Off");
-                    Toast.LENGTH_SHORT;
-                  show();
+                  Toast flashLightIsTunredOff=Toast.makeText(MainActivity.this,"FlashLight is Tunred Off");
+                  int lengthShort = Toast.LENGTH_SHORT;
                 }
             }
             catch (CameraAccessException e){
@@ -81,8 +110,4 @@ public class FlashLightController extends CameraPermissionHelper{
             }
         }
     }
-
-
-
-
 }
