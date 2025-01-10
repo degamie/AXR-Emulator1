cecb3bb4abc1f14822d54dc881db71e152b8cfe5
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-01-10 07:45:05 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Common/Helpers/DisplayRotationHelper.java
+++ app/src/main/java/com/example/axremulator2/Common/Helpers/DisplayRotationHelper.java
@@ -1,9 +1,12 @@
 package com.example.axremulator2.Common.helpers;
 
 import android.annotation.SuppressLint;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.hardware.display.DisplayManager;
 import android.view.Display;
+import android.view.Surface;
 
 import com.google.ar.core.Session;
 
@@ -57,6 +60,31 @@ public class DisplayRotationHelper {
 
         }
         return (int) aspectRatio;
+    }
+    public int  getCameraSensorToDisplayRotation(String CameraId)//To be implemented
+    {
+//        CameraCharacteristicks cameraCharactersticks=new CameraCharacteristics;
+        CameraCharacteristics cameracharacteristics=new =CameraCharacteristics;
+        try{
+            cameracharacteristics=cameraManager.getCameraCharacteristics(CameraId);
+
+        }catch(CameraAccessException e){
+           throw new  RuntimeException("Camera Characterstics Not Found!",e);
+        }
+    }
+
+    private int toDegrees(int rotation){
+        switch(rotation) {
+            case Surface.ROTATION_0:
+                return 0;
+            case Surface.ROTATION_90:
+                return 90;
+            case Surface.ROTATION_180:
+                return 180;
+                case Surface.ROTATION_270:
+                return 270;
+            default:throw new RuntimeException("Unable to find the Rotation in Degrees"+rotation);
+        }
     }
 
 }
