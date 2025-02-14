a621ac80585ada3430bf0fadcfbb9849b4e498d8
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-14 21:21:04 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/BackgroundRenderer.java
+++ app/src/main/java/com/example/axremulator2/BackgroundRenderer.java
@@ -6,6 +6,8 @@ import java.nio.FloatBuffer;
 
 public class BackgroundRenderer {
     public static final String LABEL =BackgroundRenderer.class.getSimpleName();
+    public  final String CAMERA_VERTEX_SHADER_NAME="shaders/screenquad.vert";
+    public final String CAMERA_FRAGMENT_SHADER_NAME="shaders/screenquad.frag";
     public static final Integer  COORDS_BUFFER_SIZE=2*4*4;
     public static final FloatBuffer NDC_QUAD_COORDS=ByteBuffer.allocateDirect(Integer.parseInt(COORDS_BUFFER_SIZE)).order(ByteOrder.nativeOrder()).asFloatBuffer();
     public static final FloatBuffer VIRTUAL_SCENE_TEXTURE=ByteBuffer.allocateDirect(Integer.parseInt(COORDS_BUFFER_SIZE)).order(ByteOrder.nativeOrder()).asFloatBuffer();
