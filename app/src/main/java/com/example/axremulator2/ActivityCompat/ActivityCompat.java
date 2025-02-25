f1c50f0b37fefa3dd905d9563e2ab2aa3e072efb
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-26 00:30:38 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/ActivityCompat/ActivityCompat.java
@@ -0,0 +1,17 @@
+package com.example.axremulator2.Old_PCKGS.helpers.ActivityCompat;
+
+import android.databinding.tool.Binding;
+
+public class ActivityCompat {
+    public ActivityCompat activitycompat;
+    public ActivityCompat(Binding binding){
+        this.binding=binding;
+    }
+
+    public ActivityCompat getActivitycompat(int save) {
+        save=activitycompat.finalize();
+        return save;
+//        return activitycompat;
+    }
+
+}
