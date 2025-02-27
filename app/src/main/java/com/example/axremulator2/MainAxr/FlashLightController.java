ce2ce53cd87145f26672574573dfe87be68fe10b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-02-27 23:57:36 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/MainAxr/FlashLightController.java
+++ app/src/main/java/com/example/axremulator2/MainAxr/FlashLightController.java
@@ -4,15 +4,19 @@ import static android.os.Build.VERSION_CODES.R;
 
 import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.ar.core.Camera;
 
+import java.io.IOException;
 import java.lang.reflect.Parameter;
 
 public class FlashLightController {//FlashLightController class declare
+    public String cameraId=null;
+    CameraManager cameraManager;
     public  Parameter p;//Parameters declare
     public Button Lightonn,lightoff;//Lights btn declare
     public Camera camera=new Camera();//Camera obj Declare
@@ -28,6 +32,21 @@ public class FlashLightController {//FlashLightController class declare
             camera.setParameters(p);//Setting Camera Parameters
             SurfaceTexture surfaceTexture=new SurfaceTexture();//SurfaceTexture Object Declare
             camera.setPreviewTexture (Texture);//Previewing CameraTexture
+        }
+        try{
+            cameraId= cameraManager.getCameraIdList()[0];
+            else Camera camera1=camera.open();
+            camera.Parameters p= camera1.getParameters();
+            p.setFlashMode(torch);
+            camera1.setParameters(p);
+            SurfaceTexture surfaceTexture=new SurfaceTexture(0);
+
+        }
+        try{
+            camera.setPreviewTexture(texture);
+        }
+        catch(IOException e){
+            e.printStackTrace();
         }
             if(input)camera.startPreview();//StartPreview Declare
         else{Toast.makeText("FlashLight Not found",Toast.LENGTH_SHORT().show());}//printing the flashlight's exception
