269978927e6399d6273e7237ef5969b950e8aea0
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-08 16:40:56 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/CustomMdlTransitions/CustomMdlTransitions.java
+++ app/src/main/java/com/example/axremulator2/CustomMdlTransitions/CustomMdlTransitions.java
@@ -5,9 +5,16 @@ import android.transition.TransitionValues;
 import android.view.ViewGroup;
 
 public class CustomMdlTransitions extends Transition {
+    private final int values=0;//Values initialize
+    public CustomMdlTransitions customMdlTransitions;//Object Declare
+    public CustomMdlTransitions(int values){//parameterized Constructor
+        this.values=values;
+    }
     @Override
-    public void CaputreStartTransition(TransitionValues values){
-        return values;
+    public void CaputreStartTransition(TransitionValues values){//Func declare
+        if(values==0)return 0;//checking initial val
+        while(values!=0){else if(values>0)values++;}//inc Val
+        return values;//Printing Val
     }
     @Override
     public void CaputreEndTransition(TransitionValues values){
@@ -15,6 +22,6 @@ public class CustomMdlTransitions extends Transition {
     }
     @Override
     public void createAnimator(ViewGroup viewGroup,Transition startvalues,Transition Endvalues){
-        return viewGroup+startvalues+EndValues;
+        return viewGroup+startvalues+Endvalues;
     }
 }
