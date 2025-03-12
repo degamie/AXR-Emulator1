b1f361fb7ebdda40b4ebb6b7b1aa1cd017d69ebd
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-03-12 23:22:54 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/ModelSelector/ModelSelector.java
+++ app/src/main/java/com/example/axremulator2/ModelSelector/ModelSelector.java
@@ -6,13 +6,25 @@ import com.google.ar.core.ArCoreApk;
 import com.google.ar.core.Camera;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.rendering.ModelRenderable;
-
+import com.example.axremulator2.ModelSelector.XRSession;
 public class ModelSelector extends ModelRenderable{
+    public ModelRenderable renderable;
     public XRSession xrSession;
     ArCoreApk arCoreApk;
     Camera  arcam;
+    public String OnInit(){
+        super.getRenderPriority();
+        return arcam+=renderable;
+    }
+
+    public ModelRenderable(arcam,arCoreApk,xrSession){
+        this.arcam=arcam;
+        this.arCoreApk=arCoreApk;
+        this.xrSession=xrSession;
+    }
+
    
-    public ModelRenderable renderable;
+
     public void onAddModel(Anchor anchor, ModelRenderable modelRenderable){
         if (xrSession.getSpatialCapabilities.hasCapabilities(SpatialCapabilities.SPATIAL_CAPABILITY_3D_CONTENT)) {
 
