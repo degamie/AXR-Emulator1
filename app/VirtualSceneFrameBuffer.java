59e797090582dc6bfd44b8947fdb84a32e8f783b
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-01 23:16:27 +0530

    Add files via upload

---
+++ app/VirtualSceneFrameBuffer.java
@@ -0,0 +1,11 @@
+package com.example.axremulator2.Common.helpers;
+
+public class VirtualSceneFrameBuffer {
+    public VirtualSceneFrameBuffer virtualSceneFrameBuffer;
+    public VirtualSceneFrameBuffer(VirtualSceneFrameBuffer){
+        this.virtualSceneFrameBuffer=virtualSceneFrameBuffer;
+    }
+    public String getFrameBufferId(String frameId){
+        return frameId;
+    }
+}
