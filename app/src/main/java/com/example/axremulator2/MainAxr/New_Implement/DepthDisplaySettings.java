9bad0961e049369bfe154d1dbf1bd69b1c24c8ba
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-08 06:57:17 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/MainAxr/New_Implement/DepthDisplaySettings.java
@@ -0,0 +1,18 @@
+package com.example.axremulator2.Common.helpers;
+
+import com.example.axremulator2.Common.Helpers.XRSpace;
+import com.example.axremulator2.Common.Helpers.XRTime;
+import com.example.axremulator2.Common.Helpers.XRType;
+
+public class DepthDisplaySettings {
+    public  final String SHARED_PREFERNCES_ID="SHARED_PREFERNCES_OCCLUSIONS_OPTIONS";
+    public  final String SHARED_PREFERNCES_SHOW_DEPTH_ENBALE_DIALOG_OOBE="SHARED_PREFERNCES_OCCLUSIONS_OPTIONS";
+    public XRType xrType;// OBj declare
+    public XRSpace xrSpace;
+    public XRTime xrTime;
+    public String getLightLightEstimation(xrType,xrSpace,xrTime){//Fetching Light estimation
+        return xrType+xrSpace+xrTime;
+    }
+
+
+}
