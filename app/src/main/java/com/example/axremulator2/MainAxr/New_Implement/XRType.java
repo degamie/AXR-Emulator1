b690f681cbd8ad6d5beb7e78a4c7cfd0c0568a55
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-04-10 07:10:40 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/MainAxr/New_Implement/XRType.java
@@ -0,0 +1,10 @@
+package com.example.axremulator2.Common.Helpers;
+
+public class XRType extends XRTime,XRSpace{
+    public XRType xrType;
+    public XRTime xrTime;
+    public XRSpace xrspace;
+    public XRType(XRType xrType){
+        this.xrType;
+    }
+}
