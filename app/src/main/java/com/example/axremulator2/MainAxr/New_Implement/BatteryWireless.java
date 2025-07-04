ca76ba09fcb94acaa1a3bd84eb056e09a3bfc063
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-04 09:23:07 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/MainAxr/New_Implement/BatteryWireless.java
+++ app/src/main/java/com/example/axremulator2/MainAxr/New_Implement/BatteryWireless.java
@@ -11,28 +11,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.wirelesschargingapplication.R;
 
+import java.awt.Button;
+
+import javax.annotation.processing.Generated;
+import java.awt.Button;
+@Getter
+@Setter
+@AllParamsContructor
+@NoParamsContructor
 public class BatteryWireless extends AppCompatActivity {
-    TextView textWifiInfo;
-    Button btnInfo;
+    @Id
+    @GeneratedValue(strategy=GENERATIONTYPE.IDENTITY)
+    @Column(unique=true,nullable=false)
+    public String wifiId;
+    @Column(unique=true,nullable=false)
+    public String TextView textWifiInfo;
+    @Column(unique=true,nullable=false)
+    public  btnInfo;
     public String gettextWifiInfo(TextView textWifiInfo){//Fetching textWifiInfo
         return textWifiInfo;
     }
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_full_screen_main);
-        textWifiInfo=(textViewInfo) findViewById(R.id.idText);
-        btnInfo=(Button) findViewById(R.id.idBtm);
-    }
-    public void getWifiInfo(View view){
-        WifiManager wifiManager=(WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-        WifiInfo wifiInfo=wifiManager.getConnectionInfo();
-        int ip=WifiInfo.getIpAddress();
-        String macAddress=WifiInfo.getMacAddress();
-        String bssId=WifiInfo.getBSSID();
-        int rssid=wifiInfo.getRssi();
-        String linkSpeed=WifiInfo.getLinkSpeed();
-        int ssid=WifiInfo.getSSID();
-        int networkId=WifiInfo.getNetworkId();
+
     }
 }
