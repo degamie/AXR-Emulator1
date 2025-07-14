217af78d54c55618335b8bdf34edd44d31c1b426
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-14 08:07:48 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/Service/GpuBufferService.java
@@ -0,0 +1,13 @@
+package com.example.axremulator2.MainAxr.New_Implement.Service;
+
+import com.example.axremulator2.MainAxr.New_Implement.Repository.GpuBufferRepository;
+
+@Service
+
+public class GpuBufferService {//Class Declare
+    @Autowired
+    public GpuBufferRepository gpuBufferRepository;
+    public String getAllBySize(float size){
+        return gpuBufferRepository.saveAllBySize(size) ;
+    }
+}
