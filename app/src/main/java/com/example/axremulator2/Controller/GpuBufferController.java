4f0d5f6de493eb726b489e4ea65be36e5546a403
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-14 08:08:29 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/Controller/GpuBufferController.java
@@ -0,0 +1,18 @@
+package com.example.axremulator2.MainAxr.New_Implement.Controller;
+
+import com.example.axremulator2.MainAxr.New_Implement.Service.GpuBufferService;
+
+@Controller
+
+public class GpuBufferController {//Class Declare
+    @Autowired
+     public GpuBufferService gpuBufferService;
+    @GetMapping("/savaAll/{size})
+    public String getAllBySize(float size){
+        return gpuBufferService.getAllBySize(size) ;//GpuBuffer's Size Fetch
+    }
+}
+//    public GpuBuffer(GpuBuffer gpubuffer){
+//        this.gpubuffer=gpubuffer;
+//    }
+//    public static Object INT_SIZE() {}//tbc//
