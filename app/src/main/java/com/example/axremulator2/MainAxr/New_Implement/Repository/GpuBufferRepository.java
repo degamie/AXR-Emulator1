158e702f7597698f8a284fabffd418a18d71ede8
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-19 21:38:00 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/MainAxr/New_Implement/Repository/GpuBufferRepository.java
@@ -0,0 +1,15 @@
+package com.example.axremulator2.MainAxr.New_Implement.Repository;
+
+//@Getter
+//@Setter
+//@AllParamsConstructor
+//@NoParamsConstructor
+
+import com.example.axremulator2.MainAxr.New_Implement.ModelSelector.GpuBuffer;
+@Repository
+public interface GpuBufferRepository extends JpaRepository<GpuBuffer,String> {//Interface Declare with Inherited Class
+    @Autowired
+    public GpuBuffer gpuBuffer;
+    public String saveAllBySize(float size);
+    public String findAllBySize(float size);
+}
