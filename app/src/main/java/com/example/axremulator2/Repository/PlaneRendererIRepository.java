7e9474f48d3e3378bc249ef459e22234c8d3eb85
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-20 20:24:14 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Repository/PlaneRendererIRepository.java
+++ app/src/main/java/com/example/axremulator2/Repository/PlaneRendererIRepository.java
@@ -1,4 +1,4 @@
-package com.example.axremulator2.Repository;
+package com.example.axremulator2.MainAxr.New_Implement.Repository;
 
 import com.example.axremulator2.Common.helpers.PlaneRenderer;
 
@@ -9,5 +9,6 @@ public interface PlaneRendererIRepository extends JpaRepository<PlaneRenderer,St
     public PlaneRenderer planeRenderer;
 
     public String saveAllBYIndexBuffer(String IndxBuffer);
+    public String findAllBYViewMatrix(float[] VIewMatrix);
 
 }
