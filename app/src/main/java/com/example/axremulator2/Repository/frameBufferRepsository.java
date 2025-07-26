c63ace1d3fc1f0a148bef6e0902698b2b8663be2
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-26 10:25:14 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/Repository/frameBufferRepsository.java
@@ -0,0 +1,26 @@
+package com.example.axremulator2.Common.helpers;
+
+import android.opengl.GLES30;
+
+import androidx.graphics.opengl.FrameBuffer;
+
+import com.google.android.filament.Texture;
+import com.example.axremulator2.Common.helpers.SampleRenderer;
+ import com.example.axremulator2.Common.helpers.VirtualSceneFrameBuffer;
+
+import java.util.List;
+
+@Repository
+public class frameBufferRepository extends JpaRepository<VirtualSceneFrameBuffer,String> {
+    @Autowired
+    public VirtualSceneFrameBuffer virtualSceneFrameBuffer;
+
+    public List<VirtualSceneFrameBuffer> saveAll();
+    public String getframeBufferId(Long FrameBufferId);
+    public String saveAllframeBufferName(String frameBufferName);
+}
+//DepthTexture=new Texture(
+//                    SampleRenderer renderer,
+//                    Texture.Target.TARGET_2D,
+//                    Texture.WrapMode.CLAMP_TO_EDGE,
+//                    false);
