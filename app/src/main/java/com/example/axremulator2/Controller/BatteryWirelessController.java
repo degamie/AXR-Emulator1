a682e69076779c7e94ce7ed2d4e01bd08035b799
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-07-26 23:00:59 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/Controller/BatteryWirelessController.java
@@ -0,0 +1,16 @@
+package com.example.axremulator2.MainAxr.New_Implement.Controller;
+
+import com.example.axremulator2.MainAxr.New_Implement.Service.BatteryWirelessService;
+
+@Controller
+public class BatteryWirelessController {
+    @Autowired
+    public BatteryWirelessService batteryWirelessService;
+    public String getTextWifiInfo(String textWifiInfo){
+        return batteryWirelessService.getTextWifiInfo(textWifiInfo);
+    }
+    public String getAllByBtnInfo(String btnInfo){
+        return batteryWirelessService.getAllByBtnInfo(btnInfo);
+    }
+
+}
