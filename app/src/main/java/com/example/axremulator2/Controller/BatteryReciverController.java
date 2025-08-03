4a96607a22210e06ca8a36e200fce4628f777bcf
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-08-03 18:36:42 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/Controller/BatteryReciverController.java
@@ -0,0 +1,17 @@
+package com.example.axremulator2.MainAxr.New_Implement.Service;
+
+@Controller
+public class BatteryReciverController {//Controller Class Declare
+    @Autowired
+     public BatteryReciverService batteryRecieverService ;//Injecting Service lAYER
+    public BatteryReciverServiceController BatteryReciverServiceController=null;//Initializing Controller Obj
+    @GetMapping("/save/{All}")
+    public String GetAll(){//fetching All Req. Data
+        return batteryRecieverService.saveAll();//Saving All BatteryReciverServices
+    }
+        @PostMapping("/findAll/{networkInfo}")
+        public String setAllByNetworkInfo(String networkInfo){// NetworkInfo's Retreival
+        return batteryRecieverService.setAllByNetworkInfo(networkInfo);
+    }
+
+}
