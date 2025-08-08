ced5a1989005f6e589f281047adc4ce214ff9111
Degamie_sign <degamiesign@gmail.com>
2025-08-08 09:23:58 +0530

    Seventeenth Commit

--- app/src/main/java/com/example/axremulator2/CommonHelpers/GpuBuffer.java
+++ app/src/main/java/com/example/axremulator2/CommonHelpers/GpuBuffer.java
@@ -1,4 +1,13 @@
 package com.example.axremulator2.Common.helpers;
 
+import org.intellij.lang.annotations.Identifier;
+
+@Getter
+@Setter
+@NoParamsConstructor
+@AllParamsConstructor//Parma's Methods Declare
 public class GpuBuffer {
+    @Id
+    @GeneratedValue(strategy=GENERATIONTYPE.IDENTITY)
+    public  Long GpuBufferId;//BufferID Declare
 }
