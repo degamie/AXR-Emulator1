47462b865e2080866a8dc97a230c378ff1c14269
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-08-31 21:23:41 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/MainAxr/New_Implement/Repository/SampleRendererRepository.java
@@ -0,0 +1,11 @@
+package com.example.axremulator2.MainAxr.New_Implement.Repository;
+
+import com.example.axremulator2.Common.helpers.SampleRenderer;
+
+ @Repository
+public interface SampleRendererRepository extends JpaRepository<SampleRenderer,String >{
+    @Autowired
+    public SampleRenderer samplerender;
+    public String getRenderer(int Renderer);
+
+}
