f72c02a32e30913befd6eb3f5503e906c0b50b7e
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-05 00:22:29 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/MainAxr/New_Implement/Service/BatteryWirelessService.java
@@ -0,0 +1,24 @@
+package com.example.axremulator2.MainAxr.New_Implement.Service;
+
+import com.example.axremulator2.MainAxr.New_Implement.Repository.BatteryWirelessRepository;
+
+@Service
+public class BatteryWirelessService {
+    @Autowired
+    public BatteryWirelessRepository batteryWirelessRepository;
+    public String getTextWifiInfo(String textWifiInfo) {
+        return batteryWirelessRepository.saveAllBytextWifiInfo(textWifiInfo);
+    }
+    public String getAllByBtnInfo(String btnInfo){
+        return batteryWirelessRepository.saveAllByBtnInfo(btnInfo);
+    }
+    public String findAllByBatteryType(String BatteryType){
+        return batteryWirelessRepository.findAllByBatteryType(BatteryType);
+    }
+     public String getAllByBatteryConnectStatus(String BatteryConnectStatus){
+        return batteryWirelessRepository.saveAllByBatteryConnectStatus(BatteryConnectStatus);//Fetching BatteryStatusConnetStatus
+     }
+    public String savegetAllByWireType(String WireType){
+        return batteryWirelessRepository.saveAllByWireType(WireType);
+    }
+}
