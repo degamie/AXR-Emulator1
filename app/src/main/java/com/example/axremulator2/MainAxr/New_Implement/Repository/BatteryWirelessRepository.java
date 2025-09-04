0aaa76fb988b9385bb9a41362fff31016ad152b0
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-05 00:20:46 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/MainAxr/New_Implement/Repository/BatteryWirelessRepository.java
@@ -0,0 +1,13 @@
+package com.example.axremulator2.MainAxr.New_Implement.Repository;
+
+import com.example.axremulator2.Old_PCKGS.helpers.axr_application.BatteryWireless;
+
+@Repository
+public interface BatteryWirelessRepository extends JpaRepository<BatteryWireless,String> {
+    public String saveAllBytextWifiInfo(String textWifiInfo);
+    public String findAllBytextWifiInfo(String textWifiInfo);
+    public String saveAllByBtnInfo(String btnInfo);
+    public String findAllByBatteryType(String BatteryType);
+    public String saveAllByBatteryConnectStatus(String BatteryConnectStatus);
+    public String saveAllByWireType(String WireType);
+}
