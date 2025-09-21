63b4b91841257fa539d51171d5a94c5da97b17a6
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-21 20:23:28 +0530

    Add files via upload

--- app/Js/WebXR/MainWebXR.js
+++ app/Js/WebXR/MainWebXR.js
@@ -2,3 +2,4 @@ const xrCam=new WebXRCamera("Camera1",scene,xrSessionManager);
 const direction=xrCam.getDirection(Axis.Z)+"Camera Direction in Front";
 const directionX=xrCam.getDirection(Axis.X)+"Camera Direction On Right";
 const Height=xrCam.realWorldHeight;
+xrCam.setTransformationFromNonVRCamera(xrCam,true);
