8ab34477b7b36dc30b3fc58c908c033c8ee6fb25
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-24 23:24:03 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Repository/BatteryRecieverRepository.java
+++ app/src/main/java/com/example/axremulator2/Repository/BatteryRecieverRepository.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.widget.TextView;
 
+import com.example.axremulator2.Model.BatteryReciever;
 import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BroadCastReciever;
 
 import java.util.List;
@@ -13,7 +14,12 @@ import java.util.List;
 public interface BatteryRecieverRepository  extends JpaRepository<BatteryReciever,String> {//BatteryReciever class inherititing BroadCastReciever
 
     public List<BatteryReciever> saveAll();
+    public List<BatteryReciever>findAll();
     public String saveAllByNetworkInfo(String networkInfo);
     public String findAllByNetworkInfo(String networkInfo);
+    public String saveAllByBatteryLevel(String BatteryLevel);
+    public String saveAllByNetworkInfo(String network);
+    public String saveAllConnectivityManager(ConnectivityManager connectivityManager);
+//    public String saveAllmBatteryLevel()
 }
 
