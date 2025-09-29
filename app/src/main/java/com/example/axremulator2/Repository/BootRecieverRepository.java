f502b27397f28d95cd5e7ddd52e7a5d67e0c4071
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-09-29 05:50:55 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/Repository/BootRecieverRepository.java
@@ -0,0 +1,13 @@
+package com.example.wirelesschargingapplication.repository;
+
+
+import com.example.wirelesschargingapplication.axr.AXR_Application.app.src.main.java.com.example.axr_application.BootReciever;
+@Repository
+public  interface  BootRecieverRepository extends JpaRepsoitory<BootReciever,String>  {
+ public String findAllByLogSize(Integer LOG_SIZE);
+ public String saveAllByLogSize(Integer LOG_SIZE);
+ public String saveAllByTag(String Tag);
+ public String findAllByTag(String Tag);
+ public Strign saveAllByFIle(String File);
+ public Strign findAllByFIle(String File);
+}
