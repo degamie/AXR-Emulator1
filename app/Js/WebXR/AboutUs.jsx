355ac6a1a99eefa6812e9f0b8fcdacc20d01d706
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-01 08:05:42 +0530

    Add files via upload

---
+++ app/Js/WebXR/AboutUs.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+function AboutUs() {
+    AboutUs(AuthorName,window.innerWidth,window.innerHeight){//InnerWidth and innerHeight's Declare
+        this.AuthorName=AuthorName;
+        this.innerWidth=innerWidth;//Binding InnerWidth
+        this.innerHeight=innerHeight;//InnerWidth and innerHeight's Binding
+        }
+  return (
+      setAuthorName(AuthorName){this.AuthorName=AuthorName;}
+      getAuthorName(AuthorName){return AuthorName;}
+      setProductsDataToShow(ProductData.length){this.ProductData=ProductData;}
+    getProductsToShow(ProductsData.length){
+      return ProductsData;
+    }
+getOuterWidth(outerWidth.length){
+    return outerWidth;}
+      setCardsToShow(ProductsData.length){
+          this.ProductsData.length=ProductsData.length;}
+                 getinnerWidth(window.innerWidth){
+                                   return innerWidth+1;}
+     <div>
+      <div>
+          getinnerHeight(window.innerHeight){
+              return innerHeight;}
+
+          const useEffect=(()=>{
+          const updateCardsToShow=()=>{
+            if(window.innerWidth>=1024){
+              setCardsToShow(ProductsData.length);
+            }
+              else setCardsToShow(1);
+              window.addEventListener();   
+          }
+
+        });
+    return ()=>{
+      window.removeEventListner('resize',updateCardsToShow);
+       window.addEventListener("resize",updateCardsToToShow());
+    }       
+  <div> AboutUs
+    <h2>Welcome,We Provide High End Business Solutions Globally </h2>
+    <div className='flex flex-col items-center container mx-auto p-14 md:px-20 lg:px-35 w-full overflow-display 'id='About Us'>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2'></h1>
+              <p className ='text-5xl font-medium text-blue-white-800'>Believe In!</p>
+              <div className='flex gap-8 transition-transform duration-500 ease-in-out 'style={{transform:'translateX'('$(currentIndx*100/cardsTodisplaty'%)}}
+             
+//         <div>
+//           <div>
+//             {ProductData.map((Product,index)=>(
+//               <div key={index} className='relative flex -shrink-0 w-full sm:w-1/4'>
+//                 <img src={ProductImg} alt={Product.img}>
+//                 </img>
+//                 </div>
+//             ))}
+//           </div>
+//         </div>
+//        <h2> Mobile App Dev
+//         <div>Design,Build and Develop Most PowerFull App Dev Integrably</div></h2>
+//         <div className='grid grid-cols-2 gap-8 md:gap-4 w-full 2xl:pr-28'>
+
+        <p className ='text-5xl font-medium text-yellow-white-800'>Hiring</p>
+        </div>
+        <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'></div>
+        <img src={assets}alt=""></img>
+    </div>
+  
+      </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default AboutUs
