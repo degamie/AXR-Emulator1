a11a3501c676266c4487a3601ba84ad480222ff2
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-22 09:25:57 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/axremulator2/MainAxr/New_Implement/Repository/CameraRepository.java
@@ -0,0 +1,12 @@
+package com.example.axremulator2.MainAxr.Camera;
+
+@Repository
+public interface CameraRepository extends JpaRepository<Cameramdl,String> {
+    public String saveAllByCameraState(String camerastate);
+public Cameramdl cameramdl;
+
+    public String OnRequestPermisission(String reqCode,String Permission,Boolean grantPermission);
+    public String cameraAeAutoCharacter();
+    public String saveAllByCameraLensView(Camera cameraState);
+    public String findAllByCameraLensView(Camera cameraState);
+}
