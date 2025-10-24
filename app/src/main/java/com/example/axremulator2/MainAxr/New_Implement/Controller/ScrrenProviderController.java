6ec4484599ec57c4269d363d91d9fe385b6f79d5
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-10-24 11:51:44 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/MainAxr/New_Implement/Controller/ScrrenProviderController.java
+++ app/src/main/java/com/example/axremulator2/MainAxr/New_Implement/Controller/ScrrenProviderController.java
@@ -4,12 +4,9 @@ import com.example.axremulator2.Common.helpers.FullScreenProvider;
 
 import com.example.axremulator2.MainAxr.New_Implement.Service.ScrrenProviderService;
 //import com.example.axremulator2.MainAxr.New_Implement.Repository.ScrrenProviderRepository;
-@Controller
 public class ScrrenProviderController {
     @Autowried
      public ScrrenProviderService scrrenProviderService;
-    @GetMapping("/saveAll/{ScreenName}")
-    public String getAllByScreenName(String ScreenName){return scrrenProviderService.getAllByScreenName(ScreenName);}//Fetching ScreenName
     @PostMapping("/saveAll/{ScreenId}")
     public String setAllByScreenId(String ScreenId){//ScreenId's Retrieval in Server
         return scrrenProviderRepository.findAllByScreenId(ScreenId);
