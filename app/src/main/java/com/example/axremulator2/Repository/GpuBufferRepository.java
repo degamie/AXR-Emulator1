8fb2223aba12e5cd794fdfe898585222043ecf17
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-11-13 11:06:22 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Repository/GpuBufferRepository.java
+++ app/src/main/java/com/example/axremulator2/Repository/GpuBufferRepository.java
@@ -5,23 +5,19 @@ package com.example.axremulator2.MainAxr.New_Implement.Repository;
 //@AllParamsConstructor
 //@NoParamsConstructor
 
-//WID(5/11/2025)
-
+//WID(13/11/2025)//Sarthak Mittal aka (Degamiesign)
 import com.example.axremulator2.MainAxr.New_Implement.ModelSelector.GpuBuffer;
 @Repository
 public interface GpuBufferRepository extends JpaRepository<GpuBuffer,String> {//Interface Declare with Inherited Class
     @Autowired
     public GpuBuffer gpuBuffer;
-    public String saveAllByFloatTime(float Time);
-    public void findAllByFLoatTIme(float time)
-
-    public String findAllByGpuBuffer(String gpuBuffer);
-    public String saveAllByGpuBuffer(GpuBuffer gpuBuffer);
-
     public String saveAllBySize(float size);
     public String findAllBySize(float size);
     public String updateAllByBufferSize(float Size);
     public String findAllBufferTime(String BufferTime);
     public String saveAllBufferTime(String BufferTime);//Fetching BufferTime
-    public String updateAllByBufferTime(String BufferTime);//Updating BufferTime in GpBuffer
+     public String saveAllgpuRenderer(String gpuRenderer);
+     public void findAllgpuRenderer(String gpuRenderer);//Retreiving gpuRenderer in Server
 }
+//    public String saveAllBySize(String size);
+
