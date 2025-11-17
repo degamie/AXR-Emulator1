a1bd256002bab89ee70689e3a18172492b214c86
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-11-17 11:39:54 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Repository/IndexBuffeRepositoryr.java
+++ app/src/main/java/com/example/axremulator2/Repository/IndexBuffeRepositoryr.java
@@ -5,7 +5,12 @@ import com.example.axremulator2.Common.helpers.IndexBuffer;
 
 @Repository
 public interface IndexBufferRepository extends JpaRepository<IndexBuffer,String> {
+    public  String updateAllIndexBufferId(Long IndexBufferId);//udpating Indx BufferId in Server
+    public String saveAllByIndexBufferId(String IndexBufferId);//Fetching IndxBufferId
+    public String findAllByIndexBufferId(String IndexBufferId);
     public String saveAllByBufferSize(Integer bufferSize);
     public String findAllByBufferSize(Integer bufferSize);//Finding BufferSize
+    public String saveAllByIndexNumber(String BufferNumber);
+    public String findAllByIndexNumber(String BufferNumber);//BufferNumber's Finding In IndexBuffer
 
 }
