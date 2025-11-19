ed4a404e67c92babc5c2e71829d2633551f05261
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-11-19 23:36:34 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Repository/ScrrenProviderRepository.java
+++ app/src/main/java/com/example/axremulator2/Repository/ScrrenProviderRepository.java
@@ -1,12 +1,15 @@
 package com.example.axremulator2.MainAxr.New_Implement.Repository;
 
+import com.example.axremulator2.Common.helpers.FullScreenProvider;
+
 @Repository
-public interface ScrrenProviderRepository extends JpaRepository<ScrrenProvider,String>{
+public interface ScrrenProviderRepository extends JpaRepository<FullScreenProvider,String>{
     @Autowired
      public ScrrenProvider screenprovider;
     public String saveAllByScreenId(String  ScreenId);
     public String findAllByScreenId(String ScreenId);
+    public String updateAllByScreenId(String ScreenId);
     public String saveAllByScreenName(String ScreenName);
-    public void findAllByScreenName(String ScreenName);
+    ;
 
 }
