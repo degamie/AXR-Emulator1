85095201ccb2fa5bb1eda51460b71e3e168a229e
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-11-26 01:06:46 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Repository/PlaneRendererIRepository.java
+++ app/src/main/java/com/example/axremulator2/Repository/PlaneRendererIRepository.java
@@ -1,20 +1,15 @@
 package com.example.axremulator2.MainAxr.New_Implement.Repository;
 
 import com.example.axremulator2.Common.helpers.PlaneRenderer;
-
-import javax.swing.SortOrder;
-
+//WID(26/11/2025)  Sarthak Mittal Aka  Degamiesign#1
 @Repository
 public interface PlaneRendererIRepository extends JpaRepository<PlaneRenderer,String> {
     public PlaneRendererIRepository planerepository=new PlaneRendererRepository();
     @Autowired
     public PlaneRenderer planeRenderer;
-    public String saveAllBYSortablePlanes(List<SortOrder>sortablePlanes);
-
     public String saveAllBYIndexBuffer(String IndxBuffer);
-
     public String findAllBYViewMatrix(float[] VIewMatrix);
     public String updateAllByViewMatrix(float[] ViewMatrix);
-    public String saveAllByPlaneRender(float[] PlaneRenderer);
+    public void findAllByModelViewMatrixy(float[] ModelViewMatrix);
 
 }
