b4798d2f1ffb33463f224b34640246cb9137a207
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-12-02 11:25:17 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Repository/XrSessionRepository.java
+++ app/src/main/java/com/example/axremulator2/Repository/XrSessionRepository.java
@@ -1,9 +1,10 @@
-//WID(30/11/2025)
+//WID(2/12/2025)
 @Repository
 public XrSessionRepository extends JpaRepository<XRsession,String>{
     public List<XRSession> saveAll();
     public String findAllByXrDisplaY(String flashLightID,String mdlrenderertype);
     public void saveAllByXrDisplaY(String flashLightID,String mdlrenderertype);
+    public void findAllByXrDisplaYId(String flashLightID,String mdlrenderertype);
 
 
 }
