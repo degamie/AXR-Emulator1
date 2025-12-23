064cfb40dce8e224b65425ef0b561aa5a88ef564
Degamie_sign <106448560+degamie@users.noreply.github.com>
2025-12-23 10:40:36 +0530

    Add files via upload

--- app/src/main/java/com/example/axremulator2/Repository/SampleRendererRepository.java
+++ app/src/main/java/com/example/axremulator2/Repository/SampleRendererRepository.java
@@ -2,6 +2,7 @@ package com.example.axremulator2.MainAxr.New_Implement.Repository;
 
 import com.example.axremulator2.Common.helpers.SampleRenderer;
 
+//WID(23/12/2025)(DegamieSign)
  @Repository
 public interface SampleRendererRepository extends JpaRepository<SampleRenderer,String >{
     @Autowired
@@ -13,4 +14,5 @@ public interface SampleRendererRepository extends JpaRepository<SampleRenderer,S
      public String saveAllByBufferId(String BufferId);
      public String findAllByBufferId(String BufferId);
      public void findAllByviewPortSize(float viewPortSize);
+     public String saveByviewPortSize(float viewPortSize);//Fetchign PortSize in App
 }
